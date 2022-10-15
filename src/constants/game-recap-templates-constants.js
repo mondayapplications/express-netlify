c7851c5760c2c78faab53040fc19757d09311891
liorkedem <53260347+liorkedem@users.noreply.github.com>
2022-10-15 18:41:13 +0300

    Revert "feature/liorke/copy_from_swish_bot"

--- src/constants/game-recap-templates-constants.js
+++
@@ -1,5 +0,0 @@
-const AMAZING_GAME_TEMPLATE = [
-  "${ PLAYER_NAME } popped off for a ${ PTS_PHRASE } over ${ MIN_PHRASE }, adding ${ REB_PHRASE }, ${ AST_PHRASE } and ${ STL_PHRASE }.",
-];
-
-module.exports = { AMAZING_GAME_TEMPLATE };
