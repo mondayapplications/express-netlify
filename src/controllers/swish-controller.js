1bee704aba0f666b765678f2f55aaa28ab4f2b98
Lior Kedem <liorke@monday.com>
2022-10-24 13:27:03 +0300

    first step

--- src/controllers/swish-controller.js
+++ src/controllers/swish-controller.js
@@ -1,23 +1,27 @@
+const { result } = require("lodash");
 const TeamService = require("../services/team-service");
 // const SwishService = require("../services/swish-service");
 
 class SwishController {
   static async addUpdatePlayersBasicInfo(req, res) {
-    // res.json({ lior: "kedem" });
-    let players = {};
-    const teams = await TeamService.getTeams();
-    for (const { team } of teams) {
-      const { id: teamId } = team;
-      const teamPlayers = await TeamService.getTeamPlayers(teamId);
+    try {
+      let players = {};
+      const teams = await TeamService.getTeams();
+      for (const { team } of teams) {
+        const { id: teamId } = team;
+        const teamPlayers = await TeamService.getTeamPlayers(teamId);
 
-      for (const player of teamPlayers) {
-        players[player.id] = player;
+        for (const player of teamPlayers) {
+          players[player.id] = player;
+        }
       }
+
+      const result = await SwishService.addUpdatePlayersBasicInfo(players);
+      res.json(result);
+    } catch (error) {
+      res.json(error);
+      console.log("--------this is my error----------", error);
     }
-
-    // const result = await SwishService.addUpdatePlayersBasicInfo(players);
-
-    res.json(result);
   }
 }
 
